81cf4cb5b2e6107dfe3d015b3ed36f2abbfeb771
CEO <bospan@cleverest.tech>
2018-05-18 21:15:59 +0600

    Finished, waiting day X

--- TCP Zhuldyz/one_soc_new.py
+++ TCP Zhuldyz/one_soc_new.py
@@ -6,53 +6,59 @@ import apiai
 import json
 
 
-def give_answer(question):
-    request = apiai.ApiAI('167a082419914df2b44700f2bcda6087').text_request() # Токен API к Dialogflow
-    request.lang = 'ru' # На каком языке будет послан запрос
-    request.session_id = 'ZhuldyzAIbot' # ID Сессии диалога (нужно, чтобы потом учить бота)
-    request.query = question # Посылаем запрос к ИИ с сообщением от юзера
-    responseJson = json.loads(request.getresponse().read().decode('utf-8'))
-    response = responseJson['result']['fulfillment']['speech'] # Разбираем JSON и вытаскиваем ответ
-    # Если есть ответ от бота - присылаем юзеру, если нет - бот его не понял
-    if response:
-        answer = str(responseJson['result']['fulfillment']['speech'])
-        if str(responseJson['result']['action']) == "smalltalk.user.angry":
-            emotion = "Angry"
-        elif str(responseJson['result']['action']) == "smalltalk.user.loves_agent":
-            emotion = "Sexy"
-        elif str(responseJson['result']['action']) == "smalltalk.agent.beautiful":
-            emotion = "Happy"
-        elif str(responseJson['result']['action']) == "smalltalk.greetings.bye":
-            emotion = "Suprised"
-        else:
-            try:
-                emotion = str(responseJson['result']['metadata']['intentName'])
-                print(responseJson)
-            except:
+def give_answer(question, names, types = 0):
+    if types == 0:
+        request = apiai.ApiAI('167a082419914df2b44700f2bcda6087').text_request() # Токен API к Dialogflow
+        request.lang = 'ru' # На каком языке будет послан запрос
+        request.session_id = 'ZhuldyzAIbot' # ID Сессии диалога (нужно, чтобы потом учить бота)
+        request.query = question # Посылаем запрос к ИИ с сообщением от юзера
+        responseJson = json.loads(request.getresponse().read().decode('utf-8'))
+        response = responseJson['result']['fulfillment']['speech'] # Разбираем JSON и вытаскиваем ответ
+        # Если есть ответ от бота - присылаем юзеру, если нет - бот его не понял
+        information = "clev"
+        emotion = "Happy"
+        if response:
+            answer = str(responseJson['result']['fulfillment']['speech'])
+            if str(responseJson['result']['action']) == "smalltalk.user.angry":
+                emotion = "Angry"
+            elif str(responseJson['result']['action']) == "smalltalk.user.loves_agent":
+                emotion = "Sexy"
+            elif str(responseJson['result']['action']) == "smalltalk.agent.beautiful":
                 emotion = "Happy"
+            elif str(responseJson['result']['action']) == "smalltalk.greetings.bye":
+                emotion = "Suprised"
+            else:
+                emotion = "Happy"
+            for name in names:
+                if str(responseJson['result']['metadata']['intentName']) == name:
+                    information = str(responseJson['result']['metadata']['intentName'])
+                    break
+                else:
+                    information = "clev"
+        else:
+            answer = "Простите, я Вас не поняла. Можете перефразировать, я только учусь."
+            emotion = "Thinking"
+            information = "clev"
+
+        message = emotion + "#ru-RU#" + answer
+        messagelow = information + "#None#None"
     else:
-        answer = "Простите, я Вас не поняла. Можете перефразировать, я только учусь."
-        emotion = "Thinking"
-    return answer, emotion
+        message = "Normal#ru-RU#" + question
+        messagelow = "clev#None#None"
+    return message, messagelow
 
 
-def upper_monitor():
-    #examples = ["clev#ru-RU#Здраствуйте, меня зовут Жулдыз, я робот гид", "Happy#ru-RU#Я так рада что вы тут"]
-    phrase = input("What to ask?")
-    answer, emotion = give_answer(phrase)
-    message = emotion + "#ru-RU#" + answer
-    print(message)
-    return message
+# types_off = 0 for face, 1 for boobs; types = 0 for clever answer, 1 for just say;
+def send_to_display(top_conn, bot_conn, names, phrase, types):
+    res, res2 = give_answer(phrase, names, types)
+
+    vysl = res.encode("utf8")  # encode the result string
+    top_conn.sendall(vysl)  # send it to client
+    vysl2 = res2.encode("utf8")  # encode the result string
+    bot_conn.sendall(vysl2)  # send it to client
 
 
-def lower_monitor():
-    examples = ["Kazkosmos", "KGS", "Matrix"]
-    picture = examples[random.randint(0, 3)]
-    print(picture)
-    return picture
-
-
-def client_thread(types_off, conn, ip, port, MAX_BUFFER_SIZE = 4096):
+def from_display(conn, MAX_BUFFER_SIZE = 4096):
 
     # the input is in bytes, so decode it
     input_from_client_bytes = conn.recv(MAX_BUFFER_SIZE)
@@ -65,27 +71,23 @@ def client_thread(types_off, conn, ip, port, MAX_BUFFER_SIZE = 4096):
 
     # decode input and strip the end of line
     input_from_client = input_from_client_bytes.decode("utf8").rstrip()
-    print(input_from_client)
-    if types_off == 1:
-        res = upper_monitor()
-    else:
-        res = lower_monitor()
-    print("Result of processing {} is: {}".format(input_from_client, res))
-
-    vysl = res.encode("utf8")  # encode the result string
-    conn.sendall(vysl)  # send it to client
-    # conn.close()  # close connection
-    # print('Connection ' + ip + ':' + port + " ended")
+    return input_from_client
 
 
-def start_server():
+def start_server(ip, port):
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # this is for easy starting/killing the app
     soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     print('Socket 1 created')
 
+    with open("./list.txt", "r") as listik:
+        files = listik.readlines()
+        names = []
+        for fil in files:
+            names.append(fil.replace('\n', '').replace('\r', ''))
+
     try:
-        soc.bind(("192.168.2.2", 6666))
+        soc.bind((ip, port))
         print('Socket bind complete')
     except socket.error as msg:
         print('Bind failed. Error : ' + str(sys.exc_info()))
@@ -99,15 +101,47 @@ def start_server():
     conn, addr = soc.accept()
     ip, port = str(addr[0]), str(addr[1])
     print('Accepting connection 1 from ' + ip + ':' + port)
+    return conn, names, soc
+
+
+def speak_with():
+
+    # Инициализация системы
+    top_ip = "192.168.2.2"
+    top_port = 6666
+    bot_ip = "192.168.2.2"
+    bot_port = 7777
+
+    top_conn, names, top_soc = start_server(top_ip, top_port)
+    bot_conn, names, bot_soc = start_server(bot_ip, bot_port)
+
+    if from_display(bot_conn) == "Hello":
+        print("Boobs are connected")
+        if from_display(top_conn) == "Hello":
+            print("Face is connected")
+            send_to_display(top_conn, bot_conn, names, "Инициализация всей системы, протокол Зарождение", 1)
+            if from_display(top_conn):
+                if from_display(bot_conn):
+                    send_to_display(top_conn, bot_conn, names, "Завершение протокола Зарождение, "
+                                                               "я родилась, протокол собеседник", 1)
+                    from_display(top_conn)
+                    from_display(bot_conn)
+
+    # Инициализация закончена
+
+
     while True:
         try:
-            print("First one:")
-            client_thread(1, conn, ip, port)
+            phrase = input("Phrase")
+            print("Answer")
+            send_to_display(top_conn, bot_conn, names, phrase, 0)
+            print("From face: " + str(from_display(top_conn)))
+            print("From boobs: " + str(from_display(bot_conn)))
         except:
             print("Terrible error!")
             traceback.print_exc()
-            soc.close()
+            top_soc.close()
+            bot_soc.close()
 
 
-start_server()
-
+speak_with()
--- robot.py
+++ robot.py
@@ -1,95 +1,120 @@
-import random
-import apiai
-import json
 import cv2
 import speech_recognition as sr
 import socket
-import re
-
+import sys
+import random
+import apiai
+import json
 
 # Work with lower RPI
-def go_to_position(position):
+def go_to_position(position, conn, MAX_BUFFER_SIZE = 4096):
+    sms = position.encode("utf-8")
+    conn.sendall(sms)
     return True
 
 
 # End of work with lower RPI
 
 # Interfaces block
-def connect_to_interfaces():
+def give_answer(question, names, types = 0):
+    if types == 0:
+        request = apiai.ApiAI('167a082419914df2b44700f2bcda6087').text_request() # Токен API к Dialogflow
+        request.lang = 'ru' # На каком языке будет послан запрос
+        request.session_id = 'ZhuldyzAIbot' # ID Сессии диалога (нужно, чтобы потом учить бота)
+        request.query = question # Посылаем запрос к ИИ с сообщением от юзера
+        responseJson = json.loads(request.getresponse().read().decode('utf-8'))
+        response = responseJson['result']['fulfillment']['speech'] # Разбираем JSON и вытаскиваем ответ
+        # Если есть ответ от бота - присылаем юзеру, если нет - бот его не понял
+        information = "clev"
+        emotion = "Happy"
+        if response:
+            answer = str(responseJson['result']['fulfillment']['speech'])
+            if str(responseJson['result']['action']) == "smalltalk.user.angry":
+                emotion = "Angry"
+            elif str(responseJson['result']['action']) == "smalltalk.user.loves_agent":
+                emotion = "Sexy"
+            elif str(responseJson['result']['action']) == "smalltalk.agent.beautiful":
+                emotion = "Happy"
+            elif str(responseJson['result']['action']) == "smalltalk.greetings.bye":
+                emotion = "Suprised"
+            else:
+                emotion = "Happy"
+            for name in names:
+                if str(responseJson['result']['metadata']['intentName']) == name:
+                    information = str(responseJson['result']['metadata']['intentName'])
+                    break
+                else:
+                    information = "clev"
+        else:
+            answer = "Простите, я Вас не поняла. Можете перефразировать, я только учусь."
+            emotion = "Thinking"
+            information = "clev"
 
-    # Create sockets for each RPI
-
-    # Create socket for face
-    face = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    face.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    print('Face created')
-
-    # Create socket for boobs
-    boobs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    boobs.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    print('Boobs created')
-
-    # Create socket for legs
-    legs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    legs.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    print('Legs created')
-
-    # Create list of them
-    sockets = [face, boobs, legs]
-
-
-    try:
-        face.bind(("192.168.8.104", 6666))
-        boobs.bind(("192.168.8.104", 7777))
-        legs.bind(("192.168.8.104", 5555))
-        print('Sockets bind complete')
-    except:
-        print('Bind failed. Error')
-
-
-    # Start listening on socket
-    for el in sockets:
-        el.listen(1)
-    print('Socket now listening')
-
-    conn_face, addr_face = face.accept()
-    ip_face, port_face = str(addr_face[0]), str(addr_face[1])
-    print('Accepting connection 1 from ' + ip_face + ':' + port_face)
-    conn_boobs, addr_boobs = boobs.accept()
-    ip_boobs, port_boobs = str(addr_boobs[0]), str(addr_boobs[1])
-    print('Accepting connection 2 from ' + ip_boobs + ':' + port_boobs)
-    conn_legs, addr_legs = legs.accept()
-    ip_legs, port_legs = str(addr_legs[0]), str(addr_legs[1])
-    print('Accepting connection 2 from ' + ip_legs + ':' + port_legs)
-
-    return conn_face, addr_face, ip_face, port_face, face, conn_boobs, \
-           addr_boobs, ip_boobs, port_boobs, boobs, conn_legs, addr_legs, \
-           ip_legs, port_legs, legs
+        message = emotion + "#ru-RU#" + answer
+        messagelow = information + "#None#None"
+    else:
+        message = "Normal#ru-RU#" + question
+        messagelow = "clev#None#None"
+    return message, messagelow
 
 
-def receive_interface_feedback(conn, MAX_BUFFER_SIZE = 4096):
+# types_off = 0 for face, 1 for boobs; types = 0 for clever answer, 1 for just say;
+def send_to_display(top_conn, bot_conn, names, phrase, types):
+    res, res2 = give_answer(phrase, names, types)
+
+    vysl = res.encode("utf8")  # encode the result string
+    top_conn.sendall(vysl)  # send it to client
+    vysl2 = res2.encode("utf8")  # encode the result string
+    bot_conn.sendall(vysl2)  # send it to client
+
+
+def from_display(conn, MAX_BUFFER_SIZE = 4096):
+
     # the input is in bytes, so decode it
     input_from_client_bytes = conn.recv(MAX_BUFFER_SIZE)
+
+    # MAX_BUFFER_SIZE is how big the message can be
+    # this is test if it's sufficiently big
+    siz = sys.getsizeof(input_from_client_bytes)
+    if siz >= MAX_BUFFER_SIZE:
+        print("The length of input is probably too long: {}".format(siz))
+
     # decode input and strip the end of line
     input_from_client = input_from_client_bytes.decode("utf8").rstrip()
     return input_from_client
 
 
-def send_interface_command(texts, emotion, conn):
-    res = emotion + "#" + detect_lang(texts) + "#" + texts
-    vysl = res.encode("utf8")  # encode the result string
-    conn.sendall(vysl)  # send it to client
-    return True
+def start_server(ip, port):
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # this is for easy starting/killing the app
+    soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print('Socket 1 created')
+
+    with open("./list.txt", "r") as listik:
+        files = listik.readlines()
+        names = []
+        for fil in files:
+            names.append(fil.replace('\n', '').replace('\r', ''))
+
+    try:
+        soc.bind((ip, port))
+        print('Socket bind complete')
+    except socket.error as msg:
+        print('Bind failed. Error : ' + str(sys.exc_info()))
+        sys.exit()
+
+    # Start listening on socket
+    soc.listen(1)
+    print('Socket now listening')
+    # this will make an infinite loop needed for
+    # not reseting server for every client
+    conn, addr = soc.accept()
+    ip, port = str(addr[0]), str(addr[1])
+    print('Accepting connection 1 from ' + ip + ':' + port)
+    return conn, names, soc
+
+
 # End of interfaces block
-
-
-# Additional modules
-def detect_lang(texts):
-    if bool(re.search('[а-яА-Я]', texts)):
-        return "ru-RU"
-    else:
-        return "en-GB"
-# End of additional interfaces block
 
 
 # Facial recognition block
@@ -132,34 +157,6 @@ def see_face():
 # End of facial recognition block
 
 
-
-# Speech block
-def give_answer(question):
-    request = apiai.ApiAI('167a082419914df2b44700f2bcda6087').texts_request() # Токен API к Dialogflow
-    request.lang = 'ru' # На каком языке будет послан запрос
-    request.session_id = 'ZhuldyzAIbot' # ID Сессии диалога (нужно, чтобы потом учить бота)
-    request.query = question # Посылаем запрос к ИИ с сообщением от юзера
-    responseJson = json.loads(request.getresponse().read().decode('utf-8'))
-    response = responseJson['result']['fulfillment']['speech'] # Разбираем JSON и вытаскиваем ответ
-    # Если есть ответ от бота - присылаем юзеру, если нет - бот его не понял
-    if response:
-        answer = str(responseJson['result']['fulfillment']['speech'])
-        if str(responseJson['result']['action']) == "smalltalk.agent.annoying":
-            emotion = "Angry"
-        elif str(responseJson['result']['action']) == "smalltalk.user.loves_agent":
-            emotion = "Sexy"
-        elif str(responseJson['result']['action']) == "smalltalk.agent.beautiful":
-            emotion = "Happy"
-        elif str(responseJson['result']['action']) == "smalltalk.greetings.bye":
-            emotion = "Suprised"
-        else:
-            emotion = "Happy"
-    else:
-        answer = "Простите, я Вас не поняла. Можете перефразировать, пожалуйста."
-        emotion = "Thinking"
-    return answer, emotion
-
-
 def listen_question():
     try:
         r = sr.Recognizer()
@@ -179,55 +176,42 @@ def listen_question():
     return question
 
 
-def show_emotion(emotion):
-
-
-    return True
-
-
-def say_answer(answer):
-
-
-    return True
-# End of speech block
-
-
 # Talking protocol
 def talking():
     size, position, number = recognize_face()
     print("Number of faces is " + str(number))
-    if number > 3:
-        fair_phrase = "Нас уже много, давайте начнем экскурсию! Следуйте за мной, пожалуйста."
-        say_answer(fair_phrase)
-        show_emotion("Happy")
-        protocol = 2
-        return protocol
-    elif number > 0:
-        open_phrase = "Здравствуйте, меня зовут Жулдыз, чем вам помочь?"
-        say_answer(open_phrase)
+    if number > 0:
+        open_phrase = "Задавайте вопросы, я вам помогу"
+        send_to_display(top_conn, bot_conn, names, open_phrase, 1)
+        from_display(top_conn)
+        from_display(bot_conn)
         while True:
             question = listen_question()
-            if question:
-                answer, emotion = give_answer(question)
-                is_face = see_face()
-                if is_face == 0:
-                    break
-                show_emotion(emotion)
-                say_answer(answer)
-                size, position, number = recognize_face()
-                if number < 1:
-                    break
+            if question.lower() == "начинай экскурсию":
+                return 2
+            elif question.lower() == "пора спать":
+                return 4
+            elif question.lower() == "пора работать":
+                return 5
+            elif question:
+                send_to_display(top_conn, bot_conn, names, question, 0)
+                from_display(top_conn)
+                from_display(bot_conn)
             else:
                 size, position, number = recognize_face()
                 if number < 1:
                     break
                 else:
-                    say_answer("Пожалуйста, не спешите")
+                    send_to_display(top_conn, bot_conn, names, "Пожалуйста, не спешите говорить", 1)
+                    from_display(top_conn)
+                    from_display(bot_conn)
     elif number == 0:
-        if random.randint(1, 15) == 1:
+        if random.randint(1, 50) == 1:
             catch_phrase = "Пожалуйста, подходите на экскурсию по павильону казкосмос, " \
                            "я вам всё покажу, всё расскажу."
-            say_answer(catch_phrase)
+            send_to_display(top_conn, bot_conn, names, catch_phrase, 1)
+            from_display(top_conn)
+            from_display(bot_conn)
     return 1
 # End of talking protocol
 
@@ -239,32 +223,97 @@ def setup_all():
     cap.set(3, 640)  # WIDTH
     cap.set(4, 480)  # HEIGHT
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    conn_face, addr_face, ip_face, port_face, face, conn_boobs, \
-    addr_boobs, ip_boobs, port_boobs, boobs, conn_legs, addr_legs, \
-    ip_legs, port_legs, legs = connect_to_interfaces()
-
-    return cap, face_cascade, conn_face, addr_face, ip_face, port_face, face, conn_boobs, \
-           addr_boobs, ip_boobs, port_boobs, boobs, conn_legs, addr_legs, \
-           ip_legs, port_legs, legs
+    return cap, face_cascade
 # End of setup function
 
 
 # Main function
 if __name__ == '__main__':
-    # Initialize variables
-    cap, face_cascade, conn_face, addr_face, ip_face, port_face, face, conn_boobs, \
-           addr_boobs, ip_boobs, port_boobs, boobs, conn_legs, addr_legs, \
-           ip_legs, port_legs, legs, = setup_all()
+    # Инициализация системы
+    top_ip = "192.168.2.2"
+    top_port = 6666
+    bot_ip = "192.168.2.2"
+    bot_port = 7777
+    leg_ip = "192.168.2.2"
+    leg_port = 5555
+
+    top_conn, names, top_soc = start_server(top_ip, top_port)
+    bot_conn, names, bot_soc = start_server(bot_ip, bot_port)
+    leg_conn, names, leg_soc = start_server(leg_ip, leg_port)
+
+    if from_display(bot_conn) == "Hello":
+        print("Boobs are connected")
+        if from_display(top_conn) == "Hello":
+            print("Face is connected")
+            send_to_display(top_conn, bot_conn, names, "Инициализация всей системы, протокол Зарождение", 1)
+            if from_display(top_conn):
+                if from_display(bot_conn):
+                    send_to_display(top_conn, bot_conn, names, "Завершение протокола Зарождение, "
+                                                               "я родилась, протокол собеседник", 1)
+                    from_display(top_conn)
+                    from_display(bot_conn)
+
+    # Инициализация закончена
 
     # Initialize protocol
     protocol = 1
-    # 1 = dialogues, 2 - fair, 3 - go to position A, 4 - go charge
+    # 1 = dialogues, 2 - fair, 3 - go to position A, 4 - go charge, 5 - wake up
 
     while True:
-        show_emotion("Normal")
+        dvigai = "Пожалуйста, позвольте провести экскурсию."
         if protocol == 1:
             print("Protocol 1")
             protocol = talking()
         elif protocol == 2:
-            break
+            positions = ["ab", "bc", "cd"]
+            for position in positions:
+                # Берет фразы историй
+                with open("./" + str(position) + ".txt", "r") as file:
+                    texts = file.readlines()
+                for phrase in texts:
+                    # Отправляет тебе стринг "ab", "bc" или "cd"
+                    go_to_position(position, leg_conn, MAX_BUFFER_SIZE=4096)
+                    feedback = from_display(leg_conn)
+                    while 1:
+                        if feedback == "human":
+                            send_to_display(top_conn, bot_conn, names, dvigai, 1)
+                            from_display(top_conn)
+                            from_display(bot_conn)
+                            go_to_position(position, leg_conn, MAX_BUFFER_SIZE=4096)
+                            feedback = from_display(leg_conn)
+                        else:
+                            send_to_display(top_conn, bot_conn, names, phrase, 0)
+                            from_display(top_conn)
+                            from_display(bot_conn)
+                            go_to_position(position, leg_conn, MAX_BUFFER_SIZE=4096)
+                            feedback = from_display(leg_conn)
+                            break
+            for i in range(20):
+                talking()
+            protocol = 3
+        elif protocol == 3:
+            # Отправляет тебе стринг "a"
+            position = "a"
+            go_to_position(position, leg_conn, MAX_BUFFER_SIZE=4096)
+            feedback = from_display(leg_conn)
+            while 1:
+                if feedback == "human":
+                    send_to_display(top_conn, bot_conn, names, dvigai, 1)
+                    from_display(top_conn)
+                    from_display(bot_conn)
+                    go_to_position(position, leg_conn, MAX_BUFFER_SIZE=4096)
+                    feedback = from_display(leg_conn)
+                else:
+                    protocol = 1
+                    break
+        elif protocol == 4:
+            position = "charge"
+            go_to_position(position, leg_conn, MAX_BUFFER_SIZE=4096)
+            feedback = from_display(leg_conn)
+            protocol = 1
+        elif protocol == 5:
+            position = "from_charge"
+            go_to_position(position, leg_conn, MAX_BUFFER_SIZE=4096)
+            feedback = from_display(leg_conn)
+            protocol = 1
 
